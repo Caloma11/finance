9d9bd84c092723c1af36c6c084f8da9b6bac7989
gianluca <perronegian@hotmail.com>
2020-04-05 23:00:26 +0800

    session debugging1

--- helpers.py
+++ helpers.py
@@ -32,7 +32,9 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        print(session)
         if session.get("user_id") is None:
+            print("was none")
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
