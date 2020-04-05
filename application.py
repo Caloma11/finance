6ca090aa0d54bab8a08401e5230ed23052526a0f
gianluca <perronegian@hotmail.com>
2020-04-05 15:31:02 +0000

    added redis

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
 import decimal
 import re
+import redis
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
@@ -36,7 +37,7 @@ app.jinja_env.filters["usd"] = usd
 
 # app.config["SESSION_FILE_DIR"] = 'redis'
 # app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "redis"
+app.config["SESSION_TYPE"] = "<redis></redis>"
 
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Session(app)
