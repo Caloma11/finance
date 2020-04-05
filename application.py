dcf6821e627d1fee368a0d3f350883ee880ef836
gianluca <perronegian@hotmail.com>
2020-04-05 15:21:07 +0000

    set secret key as enviroment variable

--- application.py
+++ application.py
@@ -38,7 +38,7 @@ app.jinja_env.filters["usd"] = usd
 # app.config["SESSION_PERMANENT"] = False
 # app.config["SESSION_TYPE"] = "filesystem"
 
-app.config['SECRET_KEY'] = 'super secret key'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Session(app)
 
 # Configure database
