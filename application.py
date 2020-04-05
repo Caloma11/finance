b6cd59f2500c537a16f900d11f028167e55d7873
gianluca <perronegian@hotmail.com>
2020-04-05 22:54:08 +0800

    allowed caching

--- application.py
+++ application.py
@@ -22,12 +22,12 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+# @app.after_request
+# def after_request(response):
+#     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     response.headers["Expires"] = 0
+#     response.headers["Pragma"] = "no-cache"
+#     return response
 
 # Custom filter
 app.jinja_env.filters["usd"] = usd
