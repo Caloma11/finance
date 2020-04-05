ed2020391d587d11d7a38faea733fb5b167fcec3
gianluca <perronegian@hotmail.com>
2020-04-05 15:12:15 +0000

    changed session storage type

--- application.py
+++ application.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Ensure responses aren't cached
+# Ensure responses aren't cached (commented out for production)
 # @app.after_request
 # def after_request(response):
 #     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -32,10 +32,11 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Custom filter
 app.jinja_env.filters["usd"] = usd
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+# Configure session to use filesystem (instead of signed cookies) (commented out for production)
+
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure database
